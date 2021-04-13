356ec785c5a3e0bbff8f010925cdf52b7a33d36d
Pieter van Goor <pieter.vangoor@anu.edu.au>
2021-04-13 15:02:21 +1000

    Update CSVReader code

--- eqf_vio/include/eqf_vio/CSVReader.h
+++ eqf_vio/include/eqf_vio/CSVReader.h
@@ -1,12 +1,23 @@
+#pragma once
+
+#include <deque>
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <type_traits>
 #include <vector>
+
+#include "eigen3/Eigen/Core"
 
 class CSVLine {
   protected:
     friend class CSVReader;
-    std::vector<std::string> data;
+    template <typename Derived> friend CSVLine& operator>>(CSVLine& line, Eigen::MatrixBase<Derived>& a);
+    template <typename Derived> friend CSVLine& operator<<(CSVLine& line, const Eigen::MatrixBase<Derived>& a);
+    template <typename Derived> friend CSVLine& operator>>(CSVLine& line, Eigen::QuaternionBase<Derived>& q);
+    template <typename Derived> friend CSVLine& operator<<(CSVLine& line, const Eigen::QuaternionBase<Derived>& q);
+
+    std::deque<std::string> data;
 
     void readLine(std::istream& lineStream) {
         data.clear();
@@ -20,8 +31,62 @@ class CSVLine {
     CSVLine() = default;
     CSVLine(std::istream& lineStream) { readLine(lineStream); }
     std::string operator[](const size_t& idx) const { return data[idx]; }
-    size_t size() { return data.size(); }
+    size_t size() const { return data.size(); }
+
+    template <typename T> CSVLine(const T& d) { *this << d; }
+
+    template <typename T, std::enable_if_t<std::is_arithmetic<T>::value, bool> = true> CSVLine& operator>>(T& d) {
+        std::stringstream(data.front()) >> d;
+        data.pop_front();
+        return *this;
+    }
+
+    template <typename T, std::enable_if_t<std::is_arithmetic<T>::value, bool> = true> CSVLine& operator<<(const T& d) {
+        std::stringstream ss;
+        ss << d;
+        data.emplace_back(ss.str());
+        return *this;
+    }
+
+    CSVLine& operator<<(const std::stringstream& ss) {
+        data.emplace_back(ss.str());
+        return *this;
+    }
 };
+
+inline std::ostream& operator<<(std::ostream& os, const CSVLine& line) {
+    for (int i = 0; i < line.size() - 1; ++i) {
+        os << line[i] << ", ";
+    }
+    os << line[line.size() - 1];
+    return os;
+}
+
+template <typename Derived> CSVLine& operator>>(CSVLine& line, Eigen::MatrixBase<Derived>& a) {
+    for (int i = 0; i < a.rows(); ++i) {
+        for (int j = 0; j < a.cols(); ++j) {
+            line >> a(i, j);
+        }
+    }
+    return line;
+}
+
+template <typename Derived> CSVLine& operator<<(CSVLine& line, const Eigen::MatrixBase<Derived>& a) {
+    for (int i = 0; i < a.rows(); ++i) {
+        for (int j = 0; j < a.cols(); ++j) {
+            line << a(i, j);
+        }
+    }
+    return line;
+}
+
+template <typename Derived> CSVLine& operator>>(CSVLine& line, Eigen::QuaternionBase<Derived>& q) {
+    return line >> q.w() >> q.x() >> q.y() >> q.z();
+}
+
+template <typename Derived> CSVLine& operator<<(CSVLine& line, const Eigen::QuaternionBase<Derived>& q) {
+    return line << q.w() << q.x() << q.y() << q.z();
+}
 
 class CSVReader {
   protected:
