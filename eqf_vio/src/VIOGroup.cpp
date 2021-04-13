7297e838784e794c3d2476b169144c53c061d644
Pieter van Goor <pieter.vangoor@anu.edu.au>
2021-04-14 08:33:18 +1000

    Merge branch 'license'

--- eqf_vio/src/VIOGroup.cpp
+++ eqf_vio/src/VIOGroup.cpp
@@ -1,3 +1,20 @@
+// Copyright (C) 2021 Pieter van Goor
+// 
+// This file is part of EqF VIO.
+// 
+// EqF VIO is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// EqF VIO is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with EqF VIO.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "eqf_vio/VIOGroup.h"
 
 using namespace Eigen;
--- eqf_vio/test/test_VIOLift.cpp
+++ eqf_vio/test/test_VIOLift.cpp
@@ -1,19 +1,19 @@
-/*
-    This file is part of GIFT.
-
-    GIFT is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    GIFT is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with GIFT.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright (C) 2021 Pieter van Goor
+// 
+// This file is part of EqF VIO.
+// 
+// EqF VIO is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// EqF VIO is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with EqF VIO.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "eigen3/Eigen/Dense"
 #include "eqf_vio/EqFMatrices.h"
